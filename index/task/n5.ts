43603baf073e0510ba8b178f8a29538adb0c697d
karter_gun <diasbazhanov@mail.ru>
2024-09-17 16:12:21 +0500

    task 9!!

--- index/task/n5.ts
+++ index/task/n5.ts
@@ -1,5 +1,3 @@
-function repeatStr (n, s) {
-    const resultArray = Array.from({length: n},() =>s[0])
-    const resultString = resultArray.join('')
-    return resultString
+function sayHello (name) {
+    return 'Hello, ' +  name;
 }
--- index/task/n9.ts
+++ index/task/n9.ts
@@ -1,8 +1,32 @@
-function findMultiples(integer, limit) {
-    const arr = []
-    let sum = 0
-    for (let i = 1; i * integer <= limit; i++){
-        arr.push(integer * i)
-    }
-    return arr
-}
+import 'dotenv/config'
+import express, {Request, Response} from 'express'
+
+const app = express()
+
+app.get('/books', (req: Request, res: Response) => {
+    res.send(books)
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port: ${process.env.PORT}`)
+})
+
+let books = [
+    {id: 1, title: 'Algorithms', year: 2020},
+    {id: 2, title: 'SQL', year: 2021},
+    {id: 3, title: 'CSS', year: 2019},
+    {id: 4, title: 'JS', year: 2018},
+    {id: 5, title: 'Back-end - Путь Самурая', year: 2022},
+]
+
+/*
+
+7 строка неправильная. Что нужно написать в 7 строке, чтобы на клиент улетел массив книг?
+
+В ответ напишите полностью переписанную строку (без комментария).
+
+*/
+    /*
+sanitizeBody('title').trim(),
+sanitizeBody('year').toInt(),
+
+     */
